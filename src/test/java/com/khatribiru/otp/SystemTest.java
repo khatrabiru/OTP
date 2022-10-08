1e67639adc7171b64845dc4814a9d5e4b54f0388
khatribiru <bkhatri@liveperson.com>
2022-10-08 01:19:39 +0100

    Initial setup for system tests

--- src/test/java/com/khatribiru/otp/SystemTest.java
+++ src/test/java/com/khatribiru/otp/SystemTest.java
@@ -1,4 +1,31 @@
 package com.khatribiru.otp;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class SystemTest {
+    private static final String DOMAIN = "localhost:8080/api/otp";
+    private static final String CREATE_OTP = DOMAIN + "/create";
+    private static final String VERIFY_OTP = DOMAIN + "/verify/%s";
+
+    @Test
+    void createOTPSuccess() {
+
+    }
+
+    @Test
+    void createOTPFailure() {
+
+    }
+
+    @Test
+    void verifyOTPSuccess() {
+
+    }
+
+    @Test
+    void verifyOTPFailure() {
+
+    }
 }
